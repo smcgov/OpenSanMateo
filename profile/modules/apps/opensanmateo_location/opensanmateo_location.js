16902e858fb89ec52f052c7b77ccb9a11c5b1f47
e2thex <erik@e2thex.com>
2013-07-26 13:22:50 +0000

    MC-250 add js and ajax code to populate address from location in location_address field

---
+++ profile/modules/apps/opensanmateo_location/opensanmateo_location.js
@@ -0,0 +1,23 @@
+(function ($) {
+
+  Drupal.behaviors.openSanmateoLocation = {
+    attach: function (context, settings) {
+      button = $("<a class = 'osm_loc_update'>Update Address</a>");
+      $('.field-name-field-location', context).after(button);
+      $('.osm_loc_update').click(function() {
+        value = $(this).prev().find("input").val();
+        b = $(this);
+        $.get(
+          "/opensanmateo_location/ajax/" + value,
+          function(data) {
+            console.log(data);
+            b.next().find(".street-block input:first").val(data.street);
+            b.next().find("input.locality").val(data.city);
+            b.next().find("select.state").val(data.state);
+            b.next().find("input.postal-code").val(data.zip);
+        });
+      });
+    }
+  };
+
+})(jQuery);
