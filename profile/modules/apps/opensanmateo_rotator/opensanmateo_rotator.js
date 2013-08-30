6ee2e66a7e4c33d4a505eb752d75d9ca670ab756
tirdadc <tirdadc@383630.no-reply.drupal.org>
2013-08-30 18:59:02 +0000

    [SMC-43] Editor wants to create a list of content on the site that has images, teasers, and descriptions in order to create a custom promotional list

--- profile/modules/apps/opensanmateo_rotator/opensanmateo_rotator.js
+++ profile/modules/apps/opensanmateo_rotator/opensanmateo_rotator.js
@@ -6,13 +6,10 @@
       $('.field-name-field-rotator-caption').addClass('flex-caption');
       $('.field-name-field-rotator-description').addClass('flex-caption');
       
-      var controlNav = true;
-      
-      
-      
       for (var i=0; i < Drupal.settings.OpenSanMateoRotator.length; i++) {        
         $('.pane-bundle-rotator-panels-pane .field-collection-container').eq(i).addClass(Drupal.settings.OpenSanMateoRotator[i].type);
         
+        var controlNav = true;
         // Add thumbnail data only if we have a rotator
         if (Drupal.settings.OpenSanMateoRotator[i].type == 'rotator') {
           var controlNav = "thumbnails"; 
@@ -23,9 +20,16 @@
         }
       }
       
-      $('.field-collection-container.rotator, .field-collection-container.carousel').flexslider({
+      $('.field-collection-container.rotator').flexslider({
         selector: '.field-name-field-coll-rotator-item > .field-items > .field-item',
-        controlNav: controlNav,
+        controlNav: "thumbnails",
+        pauseOnHover: true,
+        slideshow: false,
+        slideshowSpeed: 4000,
+      });
+      $('.field-collection-container.carousel').flexslider({
+        selector: '.field-name-field-coll-rotator-item > .field-items > .field-item',
+        controlNav: false,
         pauseOnHover: true,
         slideshow: false,
         slideshowSpeed: 4000,
