b4cb17f31af54435568b25c7247b1db5ddef2211
e2thex <erik@e2thex.com>
2013-09-04 16:17:57 +0000

    smc-151 removed some debug and some js work on the osm_ff_remote source

--- profile/modules/custom/osm_ff_remote_source/osm_ff_remote_source.js
+++ profile/modules/custom/osm_ff_remote_source/osm_ff_remote_source.js
@@ -4,7 +4,7 @@
 Drupal.behaviors.osm_ff_remote_source = {
   attach: function (context, settings) {
 
-    $('.filefield-source-osmremotefile .view-row').click(function() {
+    $('.filefield-source-osmremotefile td').click(function() {
       val = $(this).find('.views-field-url .field-content').text();
       $(this).parents('.view-opensanmateo-media-browser').next().find('input.form-text').val(val);
     });
