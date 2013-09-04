2d561259a3f9d215bb686cf6b6d465b568662678
e2thex <erik@e2thex.com>
2013-09-04 19:37:59 +0000

    first stage in commitng change to make the view better for file look up smc-151

--- profile/modules/custom/osm_ff_remote_source/osm_ff_remote_source.js
+++ profile/modules/custom/osm_ff_remote_source/osm_ff_remote_source.js
@@ -5,7 +5,7 @@ Drupal.behaviors.osm_ff_remote_source = {
   attach: function (context, settings) {
 
     $('.filefield-source-osmremotefile td').click(function() {
-      val = $(this).find('.views-field-url .field-content').text();
+      val = $(this).find('.views-field-url').text().trim();
       $(this).parents('.view-opensanmateo-media-browser').next().find('input.form-text').val(val);
     });
   }
