fc7521c65a66e5f8817d9ee25da92459fca3b882
e2thex <erik@e2thex.com>
2013-09-11 21:55:53 +0000

    word to fix up issue of form not working on remote image selection

--- profile/modules/custom/osm_ff_remote_source/osm_ff_remote_source.js
+++ profile/modules/custom/osm_ff_remote_source/osm_ff_remote_source.js
@@ -4,6 +4,10 @@
 Drupal.behaviors.osm_ff_remote_source = {
   attach: function (context, settings) {
 
+    $('.osm-ff-move').each(function() {
+      id = $(this).attr('for');
+      $("#" + id).replaceWith($(this));
+    });
     $('.filefield-source-osmremotefile td').click(function() {
       val = $(this).find('.views-field-url').text().trim();
       $(this).parents('.field-prefix').next().val(val);
