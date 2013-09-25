9446948f392131540e92363d6ffb91b13929728e
Jake Strawn <jake@themegeeks.com>
2013-09-25 10:25:17 -0400

    SMC-269 - cleanup surrounding the dept menu

--- profile/themes/smc_base/templates/page.tpl.php
+++ profile/themes/smc_base/templates/page.tpl.php
@@ -11,59 +11,18 @@
  
 ?>
 <div id="wrapper" class="wrapper page-wrapper group">
-    <?php if ($page['highlighted']): ?>
-      <?php print render($page['highlighted']); ?>
-    <?php endif; ?>
-    <!--
-<div id="dept-switch" class="clearfix">
-    	<a class="close show_hide icon-close" href="" rel="#dept-switch"></a>
-    	<h2>Departments</h2>
-    	<ul class="group">
-  			<li><a href="">Agriculture Weights and Measures</a></li>
-  			<li><a href="">Assessor, County Clerk-Recorder &amp;&nbsp;Chief Elections Officer</a></li>
-  			<li><a href="">Board of Supervisors</a></li>
-  			<li><a href="">Child Support Services</a></li>
-  			<li><a href="">Controller</a></li>
-  			<li><a href="">Coroner</a></li>
-  			<li><a href="">County Counsel</a></li>
-  			<li><a href="">County Managers Office/Clerk of the Board</a></li>
-  			<li><a href="">Courts</a></li>
-  			<li><a href="">District Attorney</a></li>
-  			<li><a href="">Fire Protection Services</a></li>
-  			<li><a href="">Health System</a></li>
-  			<li><a href="">Housing (Department of Housing)</a></li>
-  			<li><a href="">Human Resources Department</a></li>
-  			<li><a href="">Human Services Agency</a></li>
-  			<li><a href="">Information Services Department</a></li>
-  			<li><a href="">Parks Department</a></li>
-  			<li><a href="">Planning and Building</a></li>
-  			<li><a href="">Probation</a></li>
-  			<li><a href="">Public Works</a></li>
-  			<li><a href="">SamCERA (Retirement)</a></li>
-  			<li><a href="">San Mateo County Library</a></li>
-  			<li><a href="">Sheriff's Office</a></li>
-  			<li><a href="">Tax Collector / Treasurer / Revenue Services</a></li>
-  		</ul>
-    </div>
--->
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+  <?php if ($page['highlighted']): ?>
+    <?php print render($page['highlighted']); ?>
+  <?php endif; ?>
+
   <header id="header" class="group clearfix">
       <div class="inner">
     		<a class="dept-link show_hide icon-eye" rel="#dept-switch" href="">View All Departments</a>		
     		
     		<div class="lockup group clearfix">
     			<?php if ($site_name): ?>
-      			<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><h2 class="county-name">County<span class="tiny">of</span>San Mateo</h2></a>
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><h2 id="site-name" class="dept-name"><?php print $site_name; ?></h2></a>
+      			<a href="<?php print $front_page; ?>" title="<?php print t('County of San Mateo'); ?>" rel="home"><h2 class="county-name">County<span class="tiny">of</span>San Mateo</h2></a>
+            <a href="<?php print $front_page; ?>" title="<?php print $site_name; ?>" rel="home"><h2 id="site-name" class="dept-name"><?php print $site_name; ?></h2></a>
     			<?php endif; ?>
     		</div>
 
