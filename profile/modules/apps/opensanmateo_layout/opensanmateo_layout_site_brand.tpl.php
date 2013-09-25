6d1c467b39b900e2418a647188f4ebaac0086ca0
e2thex <erik@e2thex.com>
2013-09-25 13:56:07 +0000

    SMC-417 Add new block with the logo and slogan

---
+++ profile/modules/apps/opensanmateo_layout/opensanmateo_layout_site_brand.tpl.php
@@ -0,0 +1,8 @@
+<div class='brand-image-wrapper'>
+  <a href = "/">
+    <img src = '<?php print $logo ?>' alt = 'logo for <?php print $name ?>' />
+  </a>
+</div>
+<div class = 'brand-slogan-wrapper'>
+  <?php print $slogan ?>
+</div>
