61cfb98b31d6e5794c0c12b1684e517e67f4cff7
Jake Strawn <jake@themegeeks.com>
2013-09-26 15:48:56 -0400

    SMC-342 - applying basic wysiwyg styles; still needs work

---
+++ profile/themes/smc_base/templates/sample-node-content.tpl.php
@@ -0,0 +1,46 @@
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Summum a vobis bonum voluptas dicitur. Multa sunt dicta ab antiquis de contemnendis ac despiciendis rebus humanis; Quorum sine causa fieri nihil putandum est. Istam voluptatem perpetuam quis potest praestare sapienti? Quid enim tanto opus est instrumento in optimis artibus comparandis? An nisi populari fama? Duo Reges: constructio interrete. </p>
+
+<blockquote cite='http://drupal.org'>
+	This is a blockquote, and it is really important.
+</blockquote>
+
+<h2>This is a Header 2</h2>
+<p>Non enim quaero quid verum, sed quid cuique dicendum sit. Egone non intellego, quid sit don Graece, Latine voluptas? <a href='http://drupal.org/' target='_blank'>Quis hoc dicit?</a> <a href='http://drupal.org/' target='_blank'>Tria genera bonorum;</a> <a href='http://drupal.org/' target='_blank'>Nulla erit controversia.</a> Levatio igitur vitiorum magna fit in iis, qui habent ad virtutem progressionis aliquantum. Videamus animi partes, quarum est conspectus illustrior; Oratio me istius philosophi non offendit; Ab hoc autem quaedam non melius quam veteres, quaedam omnino relicta. <a href='http://drupal.org/' target='_blank'>Falli igitur possumus.</a> </p>
+<p>Maximus dolor, inquit, brevis est. Cum salvum esse flentes sui respondissent, rogavit essentne fusi hostes. Dici enim nihil potest verius. Memini vero, inquam; </p>
+<p>Roges enim Aristonem, bonane ei videantur haec: vacuitas doloris, divitiae, valitudo; Inde igitur, inquit, ordiendum est. Sed ad haec, nisi molestum est, habeo quae velim. Bonum incolumis acies: misera caecitas. Quae cum essent dicta, discessimus. Et quidem, Cato, hanc totam copiam iam Lucullo nostro notam esse oportebit; Ubi ut eam caperet aut quando? Laboro autem non sine causa; </p>
+
+<ul>
+	<li>Ergo omni animali illud, quod appetiti positum est in eo, quod naturae est accommodatum.</li>
+	<li>Utrum igitur tibi litteram videor an totas paginas commovere?</li>
+	<li>Istam voluptatem, inquit, Epicurus ignorat?</li>
+	<li>Num igitur eum postea censes anxio animo aut sollicito fuisse?</li>
+</ul>
+
+<h3>This is a Header 3</h3>
+<p>Non enim quaero quid verum, sed quid cuique dicendum sit. Egone non intellego, quid sit don Graece, Latine voluptas? <a href='http://drupal.org/' target='_blank'>Quis hoc dicit?</a> <a href='http://drupal.org/' target='_blank'>Tria genera bonorum;</a> <a href='http://drupal.org/' target='_blank'>Nulla erit controversia.</a> Levatio igitur vitiorum magna fit in iis, qui habent ad virtutem progressionis aliquantum. Videamus animi partes, quarum est conspectus illustrior; Oratio me istius philosophi non offendit; Ab hoc autem quaedam non melius quam veteres, quaedam omnino relicta. <a href='http://drupal.org/' target='_blank'>Falli igitur possumus.</a> </p>
+<p>Maximus dolor, inquit, brevis est. Cum salvum esse flentes sui respondissent, rogavit essentne fusi hostes. Dici enim nihil potest verius. Memini vero, inquam; </p>
+<p>Roges enim Aristonem, bonane ei videantur haec: vacuitas doloris, divitiae, valitudo; Inde igitur, inquit, ordiendum est. Sed ad haec, nisi molestum est, habeo quae velim. Bonum incolumis acies: misera caecitas. Quae cum essent dicta, discessimus. Et quidem, Cato, hanc totam copiam iam Lucullo nostro notam esse oportebit; Ubi ut eam caperet aut quando? Laboro autem non sine causa; </p>
+
+<h4>This is a Header 4</h4>
+
+<p>Non enim quaero quid verum, sed quid cuique dicendum sit. Egone non intellego, quid sit don Graece, Latine voluptas? <a href='http://drupal.org/' target='_blank'>Quis hoc dicit?</a> <a href='http://drupal.org/' target='_blank'>Tria genera bonorum;</a> <a href='http://drupal.org/' target='_blank'>Nulla erit controversia.</a> Levatio igitur vitiorum magna fit in iis, qui habent ad virtutem progressionis aliquantum. Videamus animi partes, quarum est conspectus illustrior; Oratio me istius philosophi non offendit; Ab hoc autem quaedam non melius quam veteres, quaedam omnino relicta. <a href='http://drupal.org/' target='_blank'>Falli igitur possumus.</a> </p>
+<ol>
+	<li>Ergo omni animali illud, quod appetiti positum est in eo, quod naturae est accommodatum.</li>
+	<li>Utrum igitur tibi litteram videor an totas paginas commovere?</li>
+	<li>Istam voluptatem, inquit, Epicurus ignorat?</li>
+	<li>Num igitur eum postea censes anxio animo aut sollicito fuisse?</li>
+</ol>
+<p>Maximus dolor, inquit, brevis est. Cum salvum esse flentes sui respondissent, rogavit essentne fusi hostes. Dici enim nihil potest verius. Memini vero, inquam; </p>
+<p>Roges enim Aristonem, bonane ei videantur haec: vacuitas doloris, divitiae, valitudo; Inde igitur, inquit, ordiendum est. Sed ad haec, nisi molestum est, habeo quae velim. Bonum incolumis acies: misera caecitas. Quae cum essent dicta, discessimus. Et quidem, Cato, hanc totam copiam iam Lucullo nostro notam esse oportebit; Ubi ut eam caperet aut quando? Laboro autem non sine causa; </p>
+
+<h5>This is a Header 5</h5>
+
+<p>Non enim quaero quid verum, sed quid cuique dicendum sit. Egone non intellego, quid sit don Graece, Latine voluptas? <a href='http://drupal.org/' target='_blank'>Quis hoc dicit?</a> <a href='http://drupal.org/' target='_blank'>Tria genera bonorum;</a> <a href='http://drupal.org/' target='_blank'>Nulla erit controversia.</a> Levatio igitur vitiorum magna fit in iis, qui habent ad virtutem progressionis aliquantum. Videamus animi partes, quarum est conspectus illustrior; Oratio me istius philosophi non offendit; Ab hoc autem quaedam non melius quam veteres, quaedam omnino relicta. <a href='http://drupal.org/' target='_blank'>Falli igitur possumus.</a> </p>
+<p>Maximus dolor, inquit, brevis est. Cum salvum esse flentes sui respondissent, rogavit essentne fusi hostes. Dici enim nihil potest verius. Memini vero, inquam; </p>
+<p>Roges enim Aristonem, bonane ei videantur haec: vacuitas doloris, divitiae, valitudo; Inde igitur, inquit, ordiendum est. Sed ad haec, nisi molestum est, habeo quae velim. Bonum incolumis acies: misera caecitas. Quae cum essent dicta, discessimus. Et quidem, Cato, hanc totam copiam iam Lucullo nostro notam esse oportebit; Ubi ut eam caperet aut quando? Laboro autem non sine causa; </p>
+
+<h6>This is a Header 6</h6>
+
+<p>Non enim quaero quid verum, sed quid cuique dicendum sit. Egone non intellego, quid sit don Graece, Latine voluptas? <a href='http://drupal.org/' target='_blank'>Quis hoc dicit?</a> <a href='http://drupal.org/' target='_blank'>Tria genera bonorum;</a> <a href='http://drupal.org/' target='_blank'>Nulla erit controversia.</a> Levatio igitur vitiorum magna fit in iis, qui habent ad virtutem progressionis aliquantum. Videamus animi partes, quarum est conspectus illustrior; Oratio me istius philosophi non offendit; Ab hoc autem quaedam non melius quam veteres, quaedam omnino relicta. <a href='http://drupal.org/' target='_blank'>Falli igitur possumus.</a> </p>
+<p>Maximus dolor, inquit, brevis est. Cum salvum esse flentes sui respondissent, rogavit essentne fusi hostes. Dici enim nihil potest verius. Memini vero, inquam; </p>
+<p>Roges enim Aristonem, bonane ei videantur haec: vacuitas doloris, divitiae, valitudo; Inde igitur, inquit, ordiendum est. Sed ad haec, nisi molestum est, habeo quae velim. Bonum incolumis acies: misera caecitas. Quae cum essent dicta, discessimus. Et quidem, Cato, hanc totam copiam iam Lucullo nostro notam esse oportebit; Ubi ut eam caperet aut quando? Laboro autem non sine causa; </p>
