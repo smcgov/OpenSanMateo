d8e57e2428fe2f128ae6aaf3147e3a85e27a6738
Jake Strawn <jake@themegeeks.com>
2013-10-25 12:12:36 -0400

    SMC-541 - theme breadcrumb

--- profile/themes/smc_base/template.php
+++ profile/themes/smc_base/template.php
@@ -754,3 +754,16 @@ function smc_base_file_icon($variables) {
   
   return '<span class="file-type">' . $ext . '</span>';
 }
+
+function smc_base_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+
+  if (!empty($breadcrumb)) {
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+
+    $output .= '<div class="breadcrumb">' . implode('<span class="breadcrumb-separator"></span>', $breadcrumb) . '</div>';
+    return $output;
+  }
+}
