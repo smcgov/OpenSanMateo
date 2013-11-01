a7cdcc7b007192703e3f638c2d9d6112909d5b6d
Robert Bates <rbates@phase2technology.com>
2013-11-01 19:24:21 +0000

    SMC-240: Added comments, stubbed CSS/JS inclusion

---
+++ profile/modules/apps/opensanmateo_question/js/opensanmateo_question.js
@@ -0,0 +1,14 @@
+/**
+ * Contains Javascript specific to the opensanmateo_question app
+ */
+
+(function ($) {
+  /**
+   * Use Drupal behaviors instead of the standard jQuery $(document).ready() construct.
+   * https://drupal.org/node/304258
+   */
+  Drupal.behaviors.opensanmateo_question = {
+    attach: function(context, settings) {
+    }
+  };
+})(jQuery);
