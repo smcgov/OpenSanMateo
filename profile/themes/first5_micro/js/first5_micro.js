b7b691f6f134805b5d2ba9409e7a7bf88782e803
Andrew Kenmore <akenmore@smcgov.org>
2014-05-15 16:01:36 -0700

    Added First 5 Microsite theme directory and all image assets.

---
+++ profile/themes/first5_micro/js/first5_micro.js
@@ -0,0 +1,17 @@
+Drupal.smc = Drupal.smc || {};
+
+(function($) {
+  
+  Drupal.smc.customFunction = function() {
+    // custom function here
+  }
+
+  Drupal.behaviors.customDrupalBehavior = {
+    attach: function (context) {
+      
+     // jQuery here
+     
+    }
+  };
+
+})(jQuery);
--- profile/themes/lafco_micro/template.php
+++ profile/themes/lafco_micro/template.php
@@ -1,82 +1,82 @@
 <?php
 /**
- *  REPLACE ALL smc_dark_frontend with your_theme_name
+ *  REPLACE ALL lafco_micro with your_theme_name
  */
 
 
 /**
  * Implements template_preprocess_html
  */
-function smc_dark_frontend_preprocess_html(&$variables) {
+function lafco_micro_preprocess_html(&$variables) {
   
 }
 
 /**
  * Implements template_process_html
  */
-function smc_dark_frontend_process_html(&$variables) {
+function lafco_micro_process_html(&$variables) {
   
 }
 
 /**
  * Implements template_preprocess_page
  */
-function smc_dark_frontend_preprocess_page(&$variables) {
+function lafco_micro_preprocess_page(&$variables) {
   
 }
 
 /**
  * Implements template_process_page
  */
-function smc_dark_frontend_process_page(&$variables) {
+function lafco_micro_process_page(&$variables) {
   
 }
 
 /**
  * Implements template_preprocess_node
  */
-function smc_dark_frontend_preprocess_node(&$variables) {
+function lafco_micro_preprocess_node(&$variables) {
   
 }
 
 /**
  * Implements template_process_node
  */
-function smc_dark_frontend_process_node(&$variables) {
+function lafco_micro_process_node(&$variables) {
   
 }
 
 /**
  * Implements template_process_block
  */
-function smc_dark_frontend_preprocess_block(&$variables) {
+function lafco_micro_preprocess_block(&$variables) {
   
 }
 
 /**
  * Implements template_process_block
  */
-function smc_dark_frontend_process_block(&$variables) {
+function lafco_micro_process_block(&$variables) {
   
 }
 
 /**
  * Implements hook_page_alter
  */
-function smc_dark_frontend_page_alter(&$page) {
+function lafco_micro_page_alter(&$page) {
   
 }
 
 /**
  * Implements hook_css_alter
  */
-function smc_dark_frontend_css_alter(&$css) {
+function lafco_micro_css_alter(&$css) {
   
 }
 
 /**
  * Implements hook_js_alter
  */
-function smc_dark_frontend_js_alter(&$js) {
+function lafco_micro_js_alter(&$js) {
  
 }
