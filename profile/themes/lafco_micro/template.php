c1b5962b708db9a402a6a1fed9c28dd84b3d5ad2
Andrew Kenmore <akenmore@smcgov.org>
2014-05-15 13:52:43 -0700

    Added new microsite Theme for LafCo. Included all image assets as well.

---
+++ profile/themes/lafco_micro/template.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *  REPLACE ALL smc_dark_frontend with your_theme_name
+ */
+
+
+/**
+ * Implements template_preprocess_html
+ */
+function smc_dark_frontend_preprocess_html(&$variables) {
+  
+}
+
+/**
+ * Implements template_process_html
+ */
+function smc_dark_frontend_process_html(&$variables) {
+  
+}
+
+/**
+ * Implements template_preprocess_page
+ */
+function smc_dark_frontend_preprocess_page(&$variables) {
+  
+}
+
+/**
+ * Implements template_process_page
+ */
+function smc_dark_frontend_process_page(&$variables) {
+  
+}
+
+/**
+ * Implements template_preprocess_node
+ */
+function smc_dark_frontend_preprocess_node(&$variables) {
+  
+}
+
+/**
+ * Implements template_process_node
+ */
+function smc_dark_frontend_process_node(&$variables) {
+  
+}
+
+/**
+ * Implements template_process_block
+ */
+function smc_dark_frontend_preprocess_block(&$variables) {
+  
+}
+
+/**
+ * Implements template_process_block
+ */
+function smc_dark_frontend_process_block(&$variables) {
+  
+}
+
+/**
+ * Implements hook_page_alter
+ */
+function smc_dark_frontend_page_alter(&$page) {
+  
+}
+
+/**
+ * Implements hook_css_alter
+ */
+function smc_dark_frontend_css_alter(&$css) {
+  
+}
+
+/**
+ * Implements hook_js_alter
+ */
+function smc_dark_frontend_js_alter(&$js) {
+ 
+}
