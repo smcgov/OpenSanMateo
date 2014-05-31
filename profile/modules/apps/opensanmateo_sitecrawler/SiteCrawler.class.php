1353a2d2014c023e2db96658a913d5384970f70e
Sean P. O. MacCath-Moran <sean.maccath-moran@triskelemedia.com>
2014-05-31 19:21:45 -0300

    SMC-840: work in progress

---
+++ profile/modules/apps/opensanmateo_sitecrawler/SiteCrawler.class.php
@@ -0,0 +1,91 @@
+<?php
+
+include_once('PHPCrawl_082/libs/PHPCrawler.class.php');
+
+class SiteCrawler extends PHPCrawler {
+  protected $crawler_id = 0;
+  
+  public $nodes_created = array();
+  public $nodes_updated = array();
+  
+  public $urls_processed = array();
+  
+  /**
+   * Initiates a new crawler.
+   */
+  public function __construct($crawler_id) { 
+    $this->crawler_id = $crawler_id;
+    // delete all nodes with that crawler id? only delete ones that aren't newly imported?
+    parent::__construct();
+  }
+
+  function handleDocumentInfo($DocInfo) {
+    $this->urls_processed[$DocInfo->http_status_code][] = $DocInfo->url;
+    
+    if (200 != $DocInfo->http_status_code) {
+      return;
+    }
+    
+    $nid = db_select('field_data_field_sitecrawler_url', 'fdfsu')
+      ->fields('fdfsu', array('entity_id'))
+      ->condition('fdfsu.field_sitecrawler_url_url', $DocInfo->url)
+      ->execute()
+      ->fetchField();
+  
+    if (!!$nid) {
+      $node = node_load($nid);
+      $this->nodes_updated++;
+    }
+    else {
+      $node = new stdClass();
+      $node->type = 'sitecrawler_page';
+      node_object_prepare($node);
+      $this->nodes_created++;
+    }
+    
+    $node->title = 
+      preg_match('#<head.*?<title>(.*?)</title>.*?</head>#is', $DocInfo->source, $matches)
+      ? $matches[1]
+      : $DocInfo->url;
+      
+    $node->language = LANGUAGE_NONE;
+
+    $node->field_sitecrawler_url[$node->language][0]['url'] = $DocInfo->url;
+      
+    $node->field_sitecrawler_summary[$node->language][0]['value'] = 
+      preg_match('#<body.*?>(.*?)</body>#is', $DocInfo->source, $matches)
+      ? $matches[1]
+      : $DocInfo->source;
+      
+    // store the Drupal crawler ID from the opensanmateo_sitecrawler_sites table
+    $node->field_sitecrawler_id[$node->language][0]['value'] = $this->crawler_id;
+    
+    // store the PHPCrawler ID for this pull of the site
+    $node->field_sitecrawler_instance_id[$node->language][0]['value'] = $this->getCrawlerId();
+  
+    node_save($node);
+    
+    $this->{'nodes_' . (!!$nid ? 'updated' : 'created')}[$node->nid] = $node->title . ' :: ' . $DocInfo->url;
+  }
+  
+  /**
+   * Return present abort status.
+   *
+   * @return int NULL if the process hasn't been aborted yet, otherwise one of the PHPCrawlerAbortReasons::ABORTREASON-constants.
+   */
+  public function getCurrentAbortStatus() {
+    $crawler_status = $this->CrawlerStatusHandler->getCrawlerStatus();
+    return $crawler_status->abort_reason;
+  }
+  
+  /**
+   * Return present abort status.
+   *
+   * @return int NULL if the process hasn't been aborted yet, otherwise one of the PHPCrawlerAbortReasons::ABORTREASON-constants.
+   */
+  public function getPreviousAbortStatus() {
+    $crawler_status = $this->CrawlerStatusHandler->getCrawlerStatus();
+    return $crawler_status->previous_abort_reason;
+  }
+  
+}
