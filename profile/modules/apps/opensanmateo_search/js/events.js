adb81caa6972a246681cd655d50213a85ac01947
Jesse Hofmann-Smith <jhofmann-smith@phase2technology.com>
2015-08-12 22:51:55 -0700

    fix js for expanding/collapsing calendar events

--- profile/modules/apps/opensanmateo_search/js/events.js
+++ profile/modules/apps/opensanmateo_search/js/events.js
@@ -5,35 +5,9 @@
 (function ($) {
   Drupal.behaviors.opensearch_events = {
     attach: function (context, settings) {
-      $('.view-opensanmateo-search .read-more-wrapper').bind( "click", function() {
-        event.stopPropagation();
-        $(this).hide();
-        $(this).parent().children('.event-more-info').show();
-        $(this).parent().find('.node-teaser').show();
-        return false;
-      });
-  
-      $('.view-opensanmateo-search .event-hide-more-info').bind( "click", function() {
-        event.stopPropagation();
-        $(this).parent().hide();
-        $(this).parent().parent().find('.node-teaser').hide();
-        $(this).parent().parent().children('.read-more-wrapper').show();
-        return false;
-      });
-
-      $(".view-opensanmateo-search .views-row").click(function(){
-        $(this).addClass('views-row-selected');
-        $(this).find('.event-more-info').show();
-        $(this).find('.node-teaser').show();
-	$(this).find('.aggregated-node-thumbnail').addClass('aggregated-node-thumbnail-selected');
-      });
-
-      $(".view-opensanmateo-search .views-row .event-hide-more-info a").click(function(){
-        $(this).parent().parent().parent().parent().removeClass('views-row-selected');
-        $(this).parent().parent().hide();
-        $(this).parent().parent().parent().find('.node-teaser').hide();
-        $(this).parent().parent().parent().find('.aggregated-node-thumbnail').removeClass('aggregated-node-thumbnail-selected');
-
+      // Clicking a row gives it a class.
+      $(".view-opensanmateo-search .views-row").click(function(e){
+        $(this).toggleClass('views-row-selected');
       });
     }
   };
