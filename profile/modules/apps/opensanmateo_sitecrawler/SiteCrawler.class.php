1fbed1f4289c57e888844f4e1b619c8ebf85b2de
Jesse Hofmann-Smith <jhofmann-smith@phase2technology.com>
2015-09-22 16:04:10 -0700

    use filtered_html for sitecrawler pages, remove script/style tags, silence DOMDoc warnings

--- profile/modules/apps/opensanmateo_sitecrawler/SiteCrawler.class.php
+++ profile/modules/apps/opensanmateo_sitecrawler/SiteCrawler.class.php
@@ -39,13 +39,11 @@ class SiteCrawler extends PHPCrawler {
   
     if (!!$nid) {
       $node = node_load($nid);
-      $this->nodes_updated++;
     }
     else {
       $node = new stdClass();
       $node->type = 'sitecrawler_page';
       node_object_prepare($node);
-      $this->nodes_created++;
     }
     
     $node->title = 
@@ -57,13 +55,22 @@ class SiteCrawler extends PHPCrawler {
 
     $node->field_sitecrawler_url[$node->language][0]['title'] = $node->title;
     $node->field_sitecrawler_url[$node->language][0]['url'] = $DocInfo->url;
-      
-//     $node->field_sitecrawler_summary[$node->language][0]['value'] = 
 
-
-// drupal_set_message('<pre style="border: 1px solid red;">body_xpaths: ' . print_r($this->body_xpaths,1) . '</pre>');
     $doc = new DOMDocument();
-    $doc->loadHTML($DocInfo->source);
+    // This line throws an error if there is malformed HTML. Use a source
+    // validator to correct it. Ex:
+    // * An unencoded ampersand
+    // * An unexpected element inside another, like a <table> inside a <p>
+    // * Multiple identical ID attributes on the same page.
+    // * Invalid tags based on the specified Doctype.
+    // The @ sign disables error reporting.
+    @$doc->loadHTML($DocInfo->source);
+    $doc->preserveWhiteSpace = FALSE;
+
+    removeElementsByTagName('script', $doc);
+    removeElementsByTagName('style', $doc);
+    removeElementsByTagName('link', $doc);
+
     foreach($this->body_xpaths as $body_xpath) {
       $xpath = new DOMXpath($doc);
       // $body = $xpath->query('/html/body');
@@ -71,27 +78,29 @@ class SiteCrawler extends PHPCrawler {
       $body = $xpath->query($body_xpath);
       if (!is_null($body)) {
         foreach ($body as $i => $element) {
-          $node_body = $element->nodeValue;
+          $node_body = trim($element->nodeValue);
           if (!empty($node_body)) {
             break 2;
           }
         }
       }
+      else {
+        watchdog('OpenSanMateo Site Crawler', 'No body content was found. Message: %message', array('%message' => 'This URL did not have body content: ' . $DocInfo->url));
+      }
     }
-
+    // This page doesn't have the selectors of a standard page. It's likely a
+    // landing page or home page that doesn't follow the standard page content
+    // xpath rule. Skip it.
     if (empty($node_body)) {
-      $node_body = 
-        preg_match('#<body.*?>(.*?)</body>#is', $DocInfo->source, $matches)
-        && !empty($matches[1])
-        ? $matches[1]
-        : $DocInfo->source;
+      return;
     }
 
     $node_body = mb_check_encoding($node_body, 'UTF-8') ? $node_body : utf8_encode($node_body);
-    
+
     $node->body[$node->language][0]['value'] = $node_body;
     $node->body[$node->language][0]['summary'] = text_summary($node_body);
-    $node->body[$node->language][0]['format']  = filter_default_format();
+    // Filtered HTML doesn't allow script and style tags, etc.
+    $node->body[$node->language][0]['format']  = 'filtered_html';
     
       
     // store the Drupal crawler ID from the opensanmateo_sitecrawler_sites table
@@ -99,12 +108,17 @@ class SiteCrawler extends PHPCrawler {
     
     // store the PHPCrawler ID for this pull of the site
     $node->field_sitecrawler_instance_id[$node->language][0]['value'] = $this->getCrawlerId();
-  
+
     node_save($node);
-    
-    $this->{'nodes_' . (!!$nid ? 'updated' : 'created')}[$node->nid] = $node->title . ' :: ' . $DocInfo->url;
+
+    if ((bool) $nid) {
+      $this->nodes_updated++;
+    }
+    else {
+      $this->nodes_created++;
+    }
   }
-  
+
   /**
    * Return present abort status.
    *
