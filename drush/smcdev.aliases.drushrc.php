04a4ca49d88470464924eea876cef35ac5776c13
Jesse Hofmann-Smith <jhofmann-smith@phase2technology.com>
2015-10-02 16:33:13 -0700

    fix Dev site URI in alias

--- drush/smcdev.aliases.drushrc.php
+++ drush/smcdev.aliases.drushrc.php
@@ -14,8 +14,16 @@ $smc_sites = get_smc_sites(($smc_localhost == $smc_remote_host) ? $smc_docroot :
 
 // Generate alias for each agency site
 foreach ($smc_sites as $site) {
+  // Find the site name. ex: the "parks" in the "parks.smcgov.org".
+  if (strpos($site, 'smcgov.org') === 0) {
+    $site_name = 'smcgov';
+  }
+  else {
+    // "parks.smcgov.org" minus 11 characters.
+    $site_name = substr($site, 0, -11);
+  }
   $aliases[$smc_env . '.' . $site] = array(
-    'uri' => $smc_env . '.' . 'smcstg-acquia.fayze2.com',
+    'uri' => $site_name . '.' . 'smcstg-acquia.fayze2.com',
     'root' => $smc_docroot,
     'remote-host' => $smc_remote_host,
     'remote-user' => $smc_remote_user,
